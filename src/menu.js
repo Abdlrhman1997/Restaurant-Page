ae0d1a60320c3b981bd8f98734c5a9576c670a79
Abdlrhman Hegazy <abdlrhman.hegazy@yahoo.com>
2023-05-03 17:42:09 -0700

    Add items using createItem F to function createMenu

--- src/menu.js
+++ src/menu.js
@@ -1,10 +1,31 @@
 import SalsicciaPhoto from "../dist/images/pizzas/salsiccia.png";
+import GamberiPhoto from "../dist/images/pizzas/gamberi.png";
+import PepePhoto from "../dist/images/pizzas/pepe.png";
+import DisgustosoPhoto from "../dist/images/pizzas//disgustoso.png";
 
 function createMenu() {
   createItem(
     "Salsiccia",
     "Tomato sauce, Mozarella, Tomato, Homemade sausage, Garlic, Basil",
     SalsicciaPhoto
+  );
+
+  createItem(
+    "Gamberi",
+    "Tomato sauce, Mozarella, Shrimps, Feta cheese, Olives, Basil",
+    GamberiPhoto
+  );
+
+  createItem(
+    "Pepe",
+    "Tomato sauce, Mozarella, Chilli flakes, Olives, Basil",
+    PepePhoto
+  );
+
+  createItem(
+    "Disgustoso",
+    "Tomato sauce, Bacon, Pineapple, Olives, Basil",
+    DisgustosoPhoto
   );
 }
 
